f03e580a6d9ed0a6b57e496f8147f8617fe4a746
FannyLourioux <f.lourioux@gmail.com>
2020-09-29 16:35:59 +0200

    food list addition

--- src/main/java/Wadge/BackEnd/HelloController.java
+++ src/main/java/Wadge/BackEnd/HelloController.java
@@ -1,17 +1,33 @@
 package Wadge.BackEnd;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
     @CrossOrigin
-
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @RequestMapping("/food_list")
+    public ResponseEntity<Map> ReadFiles() throws java.io.IOException{
+        Path pathIngredients = Paths.get("food_list.txt");
+        List<String> ingredients = new ArrayList<>();
+        Files.lines(pathIngredients).forEach(l-> {
+            ingredients.add(l);
+        });
+        Map m = new HashMap<String, String>();
+        m.put("Food", ingredients);
+        return new ResponseEntity<Map>(m, HttpStatus.OK);
     }
-
+    //public String index() { return "Greetings from Spring Boot!"; }
 }
