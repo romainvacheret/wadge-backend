198e78406e5ab069def07751379f6647f153e1c2
FannyLourioux <f.lourioux@gmail.com>
2020-10-19 21:12:36 +0200

    read JSON

--- src/main/java/Wadge/BackEnd/HelloController.java
+++ src/main/java/Wadge/BackEnd/HelloController.java
@@ -1,33 +1,45 @@
 package Wadge.BackEnd;
 
+
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.FileReader;
+import java.util.*;
+
 
 @RestController
 public class HelloController {
 
     @CrossOrigin
     @RequestMapping("/food_list")
-    public ResponseEntity<Map> ReadFiles() throws java.io.IOException{
-        Path pathIngredients = Paths.get("food_list.txt");
-        List<String> ingredients = new ArrayList<>();
-        Files.lines(pathIngredients).forEach(l-> {
-            ingredients.add(l);
-        });
-        Map m = new HashMap<String, String>();
-        m.put("Food", ingredients);
-        return new ResponseEntity<Map>(m, HttpStatus.OK);
+    public ResponseEntity<List<Map<String,String>>> ReadFiles() throws java.io.IOException{
+        JSONParser jsonP = new JSONParser();
+        List<Map<String,String>> list = new ArrayList<>();
+        try{
+            Object obj = jsonP.parse(new FileReader("food_list.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray arr = (JSONArray) jsonObject.get("aliments");
+            Iterator<JSONObject> iterator = arr.iterator();
+            while (iterator.hasNext()) {
+                Map m = new HashMap<String, String>();
+                JSONObject aliment = iterator.next();
+                m.put("nom",aliment.get("nom"));
+                m.put("type",aliment.get("type"));
+                m.put("consommation",((ArrayList) aliment.get("consommation")).toString());
+                list.add(m);
+            }
+        }
+       catch (Exception e) {
+            e.printStackTrace();
+       }
+        return new ResponseEntity<List<Map<String,String>>>(list, HttpStatus.OK);
     }
-    //public String index() { return "Greetings from Spring Boot!"; }
 }
