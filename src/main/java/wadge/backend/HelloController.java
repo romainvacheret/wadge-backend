f347fc231b35e1ca986d2504f49381d91bf6cefe
Romain Vacheret <romain.vacheret@gmail.com>
2020-11-12 14:26:16 +0100

    Package naming convention

--- src/main/java/wadge/backend/HelloController.java
+++ src/main/java/wadge/backend/HelloController.java
@@ -1,4 +1,4 @@
-package Wadge.BackEnd;
+package wadge.backend;
 
 import java.io.FileReader;
 import java.util.ArrayList;
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import Wadge.google.Search;
+import wadge.google.Search;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
