4d3b9795fea84de47c6aa2f4eba111dc444accc1
Romain Vacheret <romain.vacheret@gmail.com>
2020-11-12 16:07:40 +0100

    Code restructuration

--- src/main/java/wadge/backend/HelloController.java
+++ src/main/java/wadge/backend/HelloController.java
@@ -1,78 +1,40 @@
 package wadge.backend;
 
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import wadge.food_list.FoodList;
 import wadge.google.Search;
+
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class HelloController {
 
     @RequestMapping("/food_list")
-    public ResponseEntity<List<Map<String,String>>> readFile() {
-        JSONParser jsonP = new JSONParser();
-        List<Map<String,String>> list = new ArrayList<>();
-        try{
-            Object obj = jsonP.parse(new FileReader("food_list.json"));
-            JSONObject jsonObject = (JSONObject) obj;
-            JSONArray arr = (JSONArray) jsonObject.get("aliments");
-            Iterator<JSONObject> iterator = arr.iterator();
-            while (iterator.hasNext()) {
-                Map m = new HashMap<String, String>();
-                JSONObject aliment = iterator.next();
-                m.put("nom",aliment.get("nom"));
-                m.put("type",aliment.get("type"));
-                m.put("consommation",( aliment.get("consommation")).toString());
-                list.add(m);
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+    public ResponseEntity<List<Map<String, Object>>> readFile() {
+        List<Map<String, Object>> list = FoodList.readFile("food_list.json");
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
-
-
     @RequestMapping(path = "/filter/{month}")
-    public ResponseEntity<List<Map<String,String>>> getMonth(@PathVariable("month") String month){
+    public ResponseEntity<List<Map<String, Object>>> getMonth(@PathVariable("month") String month){
         if (month.length() != 0) {
             month = month.toLowerCase();
         }
-        JSONParser jsonP = new JSONParser();
-        List<Map<String, String>> list = new ArrayList<>();
+
+        List<Map<String, Object>> list = null;
         try {
-            Object obj = jsonP.parse(new FileReader("food_list.json"));
-            JSONObject jsonObject = (JSONObject) obj;
-            JSONArray arr = (JSONArray) jsonObject.get("aliments");
-            Iterator<JSONObject> iterator = arr.iterator();
-            while (iterator.hasNext()) {
-                Map mapFilter = new HashMap<String, String>();
-                JSONObject aliment = iterator.next();
-                List<String> listCompare;
-                listCompare = (ArrayList) aliment.get("consommation");
-                if (listCompare.contains(month)) {
-                    mapFilter.put("nom", aliment.get("nom"));
-                    mapFilter.put("type", aliment.get("type"));
-                    list.add(mapFilter);
-                }
-            }
+             list = FoodList.foodFromMonth(month);
         } catch (Exception e) {
             e.printStackTrace();
         }
