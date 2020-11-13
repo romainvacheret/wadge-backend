d97cbe4fdfe80ed3d67526927268470e5b5c55c8
FannyLourioux <f.lourioux@gmail.com>
2020-11-13 16:19:51 +0100

    cleaning

--- src/main/java/wadge/fridge/Fridge.java
+++ src/main/java/wadge/fridge/Fridge.java
@@ -1,26 +1,16 @@
 package wadge.fridge;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 public class Fridge {
     public static void writeFridge(List<Map<String, Object>> foodlist) {
-
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
         JSONArray fridge = new JSONArray();
-        //foreach aliment dans liste
             foodlist.forEach(foodElement -> {
                 JSONObject food = new JSONObject();
                 food.put("nom", foodElement.get("nom"));
