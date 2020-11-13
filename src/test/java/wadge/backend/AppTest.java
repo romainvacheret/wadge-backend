dc417be7f2606f0a1d35adbd187b867040e19c61
Trystan Roches <44084282+trystan4@users.noreply.github.com>
2020-11-13 14:49:14 +0100

    FoodList Tests (beginning)

--- src/test/java/wadge/backend/AppTest.java
+++ src/test/java/wadge/backend/AppTest.java
@@ -4,11 +4,48 @@
 package wadge.backend;
 
 import org.testng.annotations.*;
+import wadge.food_list.FoodList;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import static org.testng.Assert.*;
 
 public class AppTest {
+    List<Map<String, Object>> list;
+    List<Map<String, Object>> empty;
+
     @Test public void appHasAGreeting() {
         // App classUnderTest = new App();
         // assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
+    @BeforeMethod
+    public void setUp(){
+        list = new ArrayList<>();
+        list = FoodList.readFile("./src/test/resources/food_list_test.json");
+    }
+    @AfterMethod
+    public void tearDown() {
+        list = null;
+        empty = null;
+    }
+    @Test
+    public void testGoodReadFile(){
+        System.out.println(list);
+        System.out.println(FoodList.readFile("./src/test/resources/food_list_test.json"));
+        assertEquals(FoodList.readFile("./src/test/resources/food_list_test.json"),list);
+    }
+    @Test
+    public void testBadStructureReadFile(){
+        assertNotEquals(FoodList.readFile("./src/test/resources/food_test_bad_structure.json"),list);
+
+    }
+//    @Test
+//    public void testNoFile() {
+//            assertEquals(FoodList.readFile("./src/test/resources/food_list_empty.json"),empty);
+//    }
+
 }
