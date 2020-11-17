2237129c229d6a3f51a6d0c4fea752726326452a
Trystan Roches <44084282+trystan4@users.noreply.github.com>
2020-11-17 21:20:56 +0100

    Swagger

--- src/main/java/wadge/backend/Application.java
+++ src/main/java/wadge/backend/Application.java
@@ -3,7 +3,6 @@ package wadge.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
--- src/main/java/wadge/config/SpringFoxConfig.java
+++ src/main/java/wadge/config/SpringFoxConfig.java
@@ -4,10 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -22,11 +20,11 @@ public class SpringFoxConfig extends WebMvcConfigurationSupport {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("Wadge-BackEnd.src.main.java.wadge.backend"))
                 .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+                .build();
     }
     private ApiInfo apiInfo() {
         return new ApiInfo(
