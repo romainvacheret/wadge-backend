05c052e2f71caaa56d57521c81b7536f9f857f31
Trystan Roches <44084282+trystan4@users.noreply.github.com>
2020-11-18 16:14:15 +0100

    Doc Swagger -> OpenApi 3.0

--- src/main/java/wadge/backend/HelloController.java
+++ src/main/java/wadge/backend/HelloController.java
@@ -6,8 +6,6 @@ import java.util.Map;
 
 import javax.annotation.Nullable;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
@@ -22,19 +20,19 @@ import wadge.google.Search;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@Api(tags = {"sets of functions"})
+//@Api(tags = {"sets of functions"})
 public class HelloController {
 
 
     private static List<String> expirationTypes = List.of("TWO_DAYS", "FIVE_DAYS", "SEVEN_DAYS", "FORTEEN_DAYS", "EXPIRED");
 
-    @ApiOperation(value = "View the food list of fruits & vegetables")
+    //@ApiOperation(value = "View the food list of fruits & vegetables")
     @RequestMapping(path = "/food_list", method= RequestMethod.GET)
     public ResponseEntity<List<Map<String, Object>>> readFile() {
         List<Map<String, Object>> list = FoodList.readFile("food_list.json");
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
-    @ApiOperation(value = "Return a list depending on the month chosen")
+    //@ApiOperation(value = "Return a list depending on the month chosen")
     @RequestMapping(path = "/filter/{month}", method= RequestMethod.GET)
     public ResponseEntity<List<Map<String, Object>>> getMonth(@PathVariable("month") String month){
         if (month.length() != 0) {
@@ -51,7 +49,7 @@ public class HelloController {
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
-    @ApiOperation(value = "displays stores close to the user's position")
+    //@ApiOperation(value = "displays stores close to the user's position")
     @RequestMapping(path = "/map/{lat}/{lng}", method= RequestMethod.GET)
     public ResponseEntity<JSONObject> getCloseShops(@PathVariable("lat") double lat, @PathVariable("lng") double lng) {
         Search s = new Search();
@@ -61,7 +59,7 @@ public class HelloController {
         return new ResponseEntity<>(tmp, HttpStatus.OK);
     }
     @Nullable
-    @ApiOperation(value = "Alerts the user on the foods passed in parameters")
+    //@ApiOperation(value = "Alerts the user on the foods passed in parameters")
     @RequestMapping(path = "/alert/{type}", method= RequestMethod.GET)
     public ResponseEntity<List<Map<String, String>>> getExpirationAlert(@PathVariable("type") String type) {
         if(!expirationTypes.contains(type)) {
@@ -74,7 +72,7 @@ public class HelloController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @ApiOperation(value = "Alerts the user on all his foods without exceptions")
+    //@ApiOperation(value = "Alerts the user on all his foods without exceptions")
     @RequestMapping(path = "/alerts", method= RequestMethod.GET)
     public ResponseEntity<Map<String, List<Map<String, String>>>> getExpirationAlerts() {
         Map<String, List<Map<String, String>>> result = new HashMap<>();
