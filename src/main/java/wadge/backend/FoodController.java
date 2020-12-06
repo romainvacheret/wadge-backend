111a4901ccf0138e65eb129d41cadd2af6b5911f
Romain Vacheret <romain.vacheret@gmail.com>
2020-12-06 14:39:52 +0100

    refactor: add controller for month filter

--- src/main/java/wadge/backend/FoodController.java
+++ src/main/java/wadge/backend/FoodController.java
@@ -1,12 +1,15 @@
 package wadge.backend;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import wadge.food.api.Month;
 import wadge.food.impl.Food;
 import wadge.food.impl.FoodList;
 
@@ -20,5 +23,16 @@ public class FoodController {
         FoodList foodList = FoodList.getInstance();
         foodList.readFile(FOOD_LIST);
         return foodList.getFoods();
+    }
+
+    @RequestMapping(path = "/foods/{month}", method= RequestMethod.GET)
+    public List<Food> getMonth(@PathVariable("month") String month) throws Exception {
+        if (month.length() != 0) {
+            month = month.toUpperCase();
+        }
+
+        FoodList foodList = FoodList.getInstance();
+        foodList.readFile(FOOD_LIST);
+        return foodList.getFoodFromGivenMonth(Month.valueOf(month));
     }
 }
