bf8a3eafa04d1b1bb849ad806f1dabb2c68a48bf
Romain Vacheret <romain.vacheret@gmail.com>
2021-01-02 10:25:51 +0100

    test: add tests for recipes

--- src/main/java/wadge/recipe/impl/Ingredient.java
+++ src/main/java/wadge/recipe/impl/Ingredient.java
@@ -17,5 +17,36 @@ public class Ingredient {
     public String toString() {
         return "Ingredient [name=" + name + ", quantity=" + quantity + "]";
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((quantity == null) ? 0 : quantity.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Ingredient other = (Ingredient) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (quantity == null) {
+            if (other.quantity != null)
+                return false;
+        } else if (!quantity.equals(other.quantity))
+            return false;
+        return true;
+    }
     
 }
