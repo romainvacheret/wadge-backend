10008a2632d8ab4f16a281cfb73881e8412fa6c4
Romain Vacheret <romain.vacheret@gmail.com>
2021-01-02 16:12:20 +0100

    refactor: update the map's route

--- src/main/java/wadge/backend/GoogleController.java
+++ src/main/java/wadge/backend/GoogleController.java
@@ -1,14 +1,10 @@
 package wadge.backend;
 
-import java.util.List;
-import java.util.Map;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +13,11 @@ import wadge.google.Search;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-public class HelloController {  
-    @RequestMapping(path = "/map/{lat}/{lng}", method= RequestMethod.GET)
-    public ResponseEntity<JSONObject> getCloseShops(@PathVariable("lat") double lat, @PathVariable("lng") double lng) {
+public class GoogleController {  
+    @RequestMapping(path = "/map", method= RequestMethod.GET)
+    public ResponseEntity<JSONObject> getCloseShops() {
         Search s = new Search();
-        JSONObject json = s.request(lat, lng);
+        JSONObject json = s.request();
         JSONObject tmp = new JSONObject();
         tmp.put("candidates", s.parseJSON((JSONArray) json.get("candidates")));
         return new ResponseEntity<>(tmp, HttpStatus.OK);
