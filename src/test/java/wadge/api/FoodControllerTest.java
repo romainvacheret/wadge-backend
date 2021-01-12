a7679518d67d50e25aa5a0ffa8d17b8e25d772a8
Romain Vacheret <romain.vacheret@gmail.com>
2021-01-12 18:33:27 +0100

    test: add controllers tests

--- src/test/java/wadge/api/FoodControllerTest.java
+++ src/test/java/wadge/api/FoodControllerTest.java
@@ -1,25 +1,30 @@
-// package wadge.api;
+package wadge.api;
 
-// import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertTrue;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.junit.Before;
-// import org.junit.Test;
+import org.junit.Before;
+import org.junit.Test;
 
-// import wadge.model.food.Food;
+import wadge.dao.api.IFoodDao;
+import wadge.dao.impl.JsonFoodDao;
+import wadge.model.food.Food;
+import wadge.service.food.FoodService;
 
-// public class FoodControllerTest {
-//     private FoodController controller;
+public class FoodControllerTest {
+    private FoodController controller;
 
-//     @Before
-//     public void setUp() {
-//         // controller = new FoodController();
-//     }
+    @Before
+    public void setUp() {
+        IFoodDao dao = new JsonFoodDao();
+        FoodService service = new FoodService(dao);
+        controller = new FoodController(service);
+    }
 
-//     @Test
-//     public void getFridgeListTest() {
-//         // assertTrue(controller.getFridgeList() instanceof List<?>);
-//         // assertTrue(controller.getFridgeList().get(0) instanceof Food);
-//     }
-// }
+    @Test
+    public void getFridgeListTest() {
+        assertTrue(controller.getAllFood() instanceof List<?>);
+        assertTrue(controller.getAllFood().get(0) instanceof Food);
+    }
+}
