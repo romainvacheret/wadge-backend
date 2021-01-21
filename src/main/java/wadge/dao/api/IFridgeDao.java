42ebda7cafc91999b6887a1f0236ca2dcb4a2897
Romain Vacheret <romain.vacheret@gmail.com>
2021-01-21 10:00:24 +0100

    refactor: add ids to fridgefood

--- src/main/java/wadge/dao/api/IFridgeDao.java
+++ src/main/java/wadge/dao/api/IFridgeDao.java
@@ -1,10 +1,18 @@
 package wadge.dao.api;
 
 import java.util.List;
+import java.util.UUID;
 
 import wadge.model.fridge.FridgeFood;
 
 public interface IFridgeDao {
     List<FridgeFood> getAllFridge();
     boolean addAllToFridge(List<FridgeFood> food);
+    boolean insertFridgeFood(UUID id, FridgeFood food);
+
+    default boolean insertFridgeFood(FridgeFood food) {
+        UUID id = UUID.randomUUID();
+        food.setFood(id);
+        return insertFridgeFood(id, food);
+    }
 }
