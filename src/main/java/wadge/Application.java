e04843be68b4cd07c15da5e77ead42d229cdaa39
sowJamng <maodosow96@gmail.com>
2021-01-28 15:10:09 +0000

    chore: generate a json External Recipe from url

--- src/main/java/wadge/Application.java
+++ src/main/java/wadge/Application.java
@@ -2,10 +2,16 @@ package wadge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import wadge.dao.impl.JsonRecipeExtDao;
+import wadge.model.recipeExternal.RecipeExternal;
 
 @SpringBootApplication //(scanBasePackages = {"wadge.service", "wadge.dao"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        // decomenter pour voir le json sur la console
+        /*JsonRecipeExtDao r= new JsonRecipeExtDao();
+        r.RecipeExternalsFromUrl("tomate");*/
+        
     }
 }
