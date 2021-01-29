3fd2770413aaf3831c684c1f18c2f945c9714b16
sowJamng <maodosow96@gmail.com>
2021-01-29 14:03:44 +0000

    chore: add recipe external controller

--- src/main/java/wadge/api/RecipeExternalControler.java
+++ src/main/java/wadge/api/RecipeExternalControler.java
@@ -1,8 +1,12 @@
 package wadge.api;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import wadge.service.recipeexternal.RecipeExternalService;
 
+import java.util.Arrays;
+import java.util.List;
 
 
 @CrossOrigin(origins ="https://localhost:3000")
@@ -13,7 +17,11 @@ public class RecipeExternalControler {
 	RecipeExternalControler(RecipeExternalService recipeExternalService){this.recipeExternalService=recipeExternalService;}
 	
 	@PostMapping(path = "/searchRecipe/{queryingredient}")
-	public void getRecipeExternal(@PathVariable String queryingredients) {
-		this.recipeExternalService.writeRecipe(queryingredients);
+	public void getRecipeExternal(@RequestBody JsonNode queryingredients) {
+		ObjectMapper mapper = new ObjectMapper();
+		List<String> list = Arrays.asList(mapper.convertValue(queryingredients, String[].class));
+		String query = list.stream().
+				reduce((s1, s2) -> new StringBuffer(s1).append("-").append(s2).toString()).get();
+		this.recipeExternalService.writeRecipe(query);
 	}
 }
