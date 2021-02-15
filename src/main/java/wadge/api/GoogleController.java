5666255c56e4034c48b4defc3d71324003ce3d2e
TrystanR <trystan.rcs4@gmail.com>
2021-02-15 09:03:11 +0100

    refactor: clean controllers mapping

--- src/main/java/wadge/api/GoogleController.java
+++ src/main/java/wadge/api/GoogleController.java
@@ -5,8 +5,7 @@ import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import wadge.service.map.MapService;
@@ -15,7 +14,7 @@ import wadge.service.map.MapService;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class GoogleController {  
-    @RequestMapping(path = "/map", method= RequestMethod.GET)
+    @GetMapping(path = "/map")
     public ResponseEntity<JSONObject> getCloseShops() {
         MapService s = new MapService();
         JSONObject json = s.request();
