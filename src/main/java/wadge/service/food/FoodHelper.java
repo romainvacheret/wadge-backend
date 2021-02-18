fd7f28692b62311f8fff0b451635c0012ae5953c
Romain Vacheret <romain.vacheret@gmail.com>
2021-02-18 12:25:01 +0100

    chore: update version

--- src/main/java/wadge/service/food/FoodHelper.java
+++ src/main/java/wadge/service/food/FoodHelper.java
@@ -6,19 +6,19 @@ public class FoodHelper {
     private FoodHelper() {}
 
     public enum Conversion {
-        UNIT_TO_KG, KG_TO_UNIT
+        UNIT_TO_G, G_TO_UNIT
     }
 
-    private static double fromUnitToKg(Food food, double quantity) {
+    private static double fromUnitToG(Food food, double quantity) {
         return quantity * food.getWeight();
     }
 
-    private static double fromKgToUnit(Food food, double quantity) {
+    private static double fromGToUnit(Food food, double quantity) {
         return Math.ceil(quantity / food.getWeight());
     }
 
     public static double convert(Conversion type, Food food, double quantity) {
-        return type == Conversion.UNIT_TO_KG ? fromUnitToKg(food, quantity) :
-            fromKgToUnit(food, quantity);
+        return type == Conversion.UNIT_TO_G ? fromUnitToG(food, quantity) :
+            fromGToUnit(food, quantity);
     }
 }
