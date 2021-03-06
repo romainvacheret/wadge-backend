205ee2b3956aee8b09f130bb7762b8afebeb787d
Romain Vacheret <romain.vacheret@gmail.com>
2021-03-06 07:57:19 +0100

    test: add dependance injection

--- src/test/java/wadge/dao/impl/MarmitonRecipeDaoTest.java
+++ src/test/java/wadge/dao/impl/MarmitonRecipeDaoTest.java
@@ -1,21 +1,24 @@
 package wadge.dao.impl;
 
 
-import org.junit.Before;
-import org.junit.Test;
-import wadge.model.recipe.external.MarmitonRecipe;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import wadge.model.recipe.external.MarmitonRecipe;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MarmitonRecipeDaoTest {
+	@Autowired
 	private MarmitonRecipeDao marmitonRecipeDao;
 	
-	@Before
-	public void setUp(){
-		marmitonRecipeDao=new MarmitonRecipeDao();
-	}
 	@Test
 	public void getRecipefromUrlTest(){
 		assertTrue(marmitonRecipeDao.recipeExternalsFromUrl("tomate") instanceof List<?>);
