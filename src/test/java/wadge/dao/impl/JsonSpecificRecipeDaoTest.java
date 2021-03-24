90ed8a382759b3ed49dd7a13dde4289e848cfc84
TrystanR <trystan.rcs4@gmail.com>
2021-03-24 15:41:06 +0100

    chore: unused imports

--- src/test/java/wadge/dao/impl/JsonSpecificRecipeDaoTest.java
+++ src/test/java/wadge/dao/impl/JsonSpecificRecipeDaoTest.java
@@ -2,15 +2,11 @@ package wadge.dao.impl;
 
 import org.junit.Before;
 import org.junit.Test;
-import wadge.model.recipe.Recipe;
 
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class JsonSpecificRecipeDaoTest {
 	
--- src/test/java/wadge/dao/impl/MarmitonRecipeDaoTest.java
+++ src/test/java/wadge/dao/impl/MarmitonRecipeDaoTest.java
@@ -2,7 +2,6 @@ package wadge.dao.impl;
 
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.List;
