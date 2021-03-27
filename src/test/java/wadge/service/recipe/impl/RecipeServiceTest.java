26a7f141ede531cae0add2b9d97c7bda2401696c
FannyLourioux <f.lourioux@gmail.com>
2021-03-27 11:15:04 +0100

    refactor: clean unused imports

--- src/test/java/wadge/service/recipe/impl/RecipeServiceTest.java
+++ src/test/java/wadge/service/recipe/impl/RecipeServiceTest.java
@@ -4,15 +4,12 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import wadge.dao.api.IRecipeDao;
-import wadge.dao.impl.JsonRecipeDao;
 import wadge.model.recipe.Recipe;
 
 @RunWith(SpringRunner.class)
